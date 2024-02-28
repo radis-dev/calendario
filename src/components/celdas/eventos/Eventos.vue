fac8d3a30bb25b528cee1b64151c36e3aa24b7a0
Raul De Diego Diaz <radisdev30@gmail.com>
2024-02-28 22:54:14 +0100

    .

--- src/components/celdas/eventos/Eventos.vue
+++ src/components/celdas/eventos/Eventos.vue
@@ -2,6 +2,7 @@
 import type { IEvento } from '@/interfaces'
 
 import EliminarEvento from '@/components/celdas/eventos/EliminarEvento.vue'
+import ModificarEvento from '@/components/celdas/eventos/ModificarEvento.vue'
 
 interface Props {
   eventos: IEvento[]
@@ -15,6 +16,7 @@ const emitir = defineEmits(['eliminarEvento'])
   <li v-if="props.eventos.length > 0" v-for="(evento, indice) in eventos" :key="indice" class="flex flex-col space-y-4 p-4 mx-2 bg-white border border-black/10 rounded">
     <p>{{ evento.titulo }}</p>
     <div class="flex flex-col space-y-2">
+      <ModificarEvento />
       <EliminarEvento :id="evento.id!" @eliminarEvento="emitir('eliminarEvento', $event)" />
     </div>
   </li>
---
+++ src/components/celdas/eventos/ModificarEvento.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const estaAbierto = ref(false)
+const alternarMenu = () => {
+  estaAbierto.value = !estaAbierto.value
+}
+</script>
+
+<template>
+  <div>
+    <button @click="alternarMenu" class="text-white bg-orange-500 hover:bg-orange-600 p-2 rounded-b font-medium w-full">Modificar</button>
+
+    <div v-if="estaAbierto" class="fixed left-0 top-0 bg-gray-700 bg-opacity-75 w-screen h-screen">
+      <div class="bg-white rounded p-8 mx-auto my-20 w-11/12 md:w-1/2">
+        <h1 class="font-bold text-xl mb-6">Modificar evento</h1>
+        <div class="flex flex-col space-y-4">
+          <div class="flex flex-col space-y-2">
+            <label for="titulo" class="font-medium text-neutral-800">Titulo:</label>
+            <input type="text" id="titulo" class="bg-gray-100 rounded px-3.5 py-2 border border-neutral-200 focus:outline-none" />
+          </div>
+        </div>
+        <div class="flex justify-end gap-4 mt-5">
+          <button class="font-medium bg-orange-500 px-6 py-2 rounded text-white hover:bg-orange-600">Modificar</button>
+          <button @click="alternarMenu" class="font-medium px-3.5 py-2 rounded text-neutral-800 bg-gray-100 hover:bg-gray-200 transition duration-100 ease-in-out">Cancelar</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
